4ff00f619295e933b44ef67e88d764c424728298
김용일 <minerkyi@gmail.com>
2025-06-18 17:09:08 +0900

    ...

--- src/App.jsx
+++ src/App.jsx
@@ -1,19 +1,11 @@
 import { useState } from 'react';
-import Products from './pages/products/Products';
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import Index from './pages/Index';
+import Products from './pages/products/Products';
 
 function App() {
-
-  function Index() {
-    return (
-      <>
-        <h1>Index</h1>
-      </>
-    );
-  }
-
   return (
-    <BrowserRouter basename='/vite'>
+    <BrowserRouter basename='/vite/'>
       <Routes>
         <Route path="/" element={<Index />}/>
         <Route path="/products" element={<Products />}/>
