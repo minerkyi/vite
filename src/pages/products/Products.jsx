887ba32419eef1e81978ad0fac550b9a4dd09549
김용일 <minerkyi@gmail.com>
2025-06-18 15:05:20 +0900

    build setting

---
+++ src/pages/products/Products.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import './Products.css';
+
+export default function Products() {
+  return (
+    <section>
+      <h2>상품 등록</h2>
+      <p>상품 화면</p>
+    </section>
+  )
+}
