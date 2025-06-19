afbfa119c20c5e4e6f85adc605e9ab0ac28616bf
김용일 <minerkyi@gmail.com>
2025-06-19 10:11:39 +0900

    ...

--- src/pages/products/Products.jsx
+++ src/pages/products/Products.jsx
@@ -1,11 +1,13 @@
 import React from 'react';
 import './Products.css';
+import { Link } from 'react-router-dom';
 
 export default function Products() {
   return (
     <section>
       <h2>상품 등록</h2>
       <p>상품 화면</p>
+      <Link to="/">Index</Link>
     </section>
   )
 }
